063d0d106dac720acaa1b391594a3678e76f8878
Justin Underhay <jju115@telus.net>
2024-06-02 22:11:18 -0700

    Get panning working

--- WpfOcrInvoiceExtractor/MainWindow.xaml.cs
+++ WpfOcrInvoiceExtractor/MainWindow.xaml.cs
@@ -5,9 +5,10 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Media3D;
 using Ghostscript.NET.Rasterizer;
 using Tesseract;
+
+using Point = System.Windows.Point;
 
 namespace WpfOcrInvoiceExtractor
 {
@@ -16,8 +17,13 @@ namespace WpfOcrInvoiceExtractor
     /// </summary>
     public partial class MainWindow : Window
     {
-        private System.Windows.Point origin;
-        private System.Windows.Point start;
+        private Point origin;
+        private Point start;
+        private Point leftEdgeAtClick;
+        private bool disableRightPanning;
+        private bool disableLeftPanning;
+        private bool disableUpPanning;
+        private bool disableDownPanning;
 
         private double baseScale;
         public MainWindow()
@@ -41,6 +47,11 @@ namespace WpfOcrInvoiceExtractor
             invoice.MouseLeftButtonDown += image_MouseLeftButtonDown;
             invoice.MouseLeftButtonUp += image_MouseLeftButtonUp;
             invoice.MouseMove += image_MouseMove;
+
+            this.disableRightPanning = false;
+            this.disableDownPanning = false;
+            this.disableLeftPanning = false;
+            this.disableUpPanning = false;
         }
 
         //If entirety of image in view, disallow panning and outward scaling
@@ -80,20 +91,99 @@ namespace WpfOcrInvoiceExtractor
         {
             if (!invoice.IsMouseCaptured) return;
 
+            bool ignoreHTrans = false;
+            bool ignoreVTrans = false;
+
+            var ta = invoice.TransformToAncestor(imageCanvas);
+            Point areaPosition = ta.Transform(new Point(0, 0));
+
             TransformGroup transformGroup = (TransformGroup)invoice.RenderTransform;
             ScaleTransform scaleTransform = (ScaleTransform)transformGroup.Children[0];
             var tt = (TranslateTransform)transformGroup.Children[1];
 
+            Vector topLeftSide = areaPosition - new Point(0,0);
+            //Right side of image to right window edge
+            Vector bottomRightSide = new Point(imageCanvas.RenderSize.Width, imageCanvas.RenderSize.Height) 
+                - new Point(areaPosition.X + (scaleTransform.ScaleX * invoice.RenderSize.Width), areaPosition.Y + (scaleTransform.ScaleY * invoice.RenderSize.Height));
+            
             Vector v = start - e.GetPosition(imageCanvas);
-            var ta = invoice.TransformToAncestor(imageCanvas);
-            System.Windows.Point areaPosition = ta.Transform(new System.Windows.Point(0, 0));
+            if ((this.disableRightPanning && v.X < 0) || (this.disableLeftPanning && v.X > 0))
+            {
+                start.X = e.GetPosition(imageCanvas).X;
+                origin.X = tt.X;
+            }
 
-            if ((areaPosition.X + 
-                scaleTransform.ScaleX * invoice.RenderSize.Width) < this.Width && v.X > 0) return;
+            if ((this.disableUpPanning && v.Y > 0) || (this.disableDownPanning && v.Y < 0))
+            {
+                start.Y = e.GetPosition(imageCanvas).Y;
+                origin.Y = tt.Y;
+            }
 
-            //if (areaPosition.X > 0 && v.X > 0) return;
-            tt.X = origin.X - v.X;
-            tt.Y = origin.Y - v.Y;
+            //If image is not scaled enough to overflow at edges, return
+            ignoreHTrans = topLeftSide.X >= 0 && bottomRightSide.X >= 0;
+            ignoreVTrans = topLeftSide.Y >= 0 && bottomRightSide.Y >= 0;
+
+            this.disableRightPanning = topLeftSide.X >= 0;
+            this.disableLeftPanning = bottomRightSide.X >= 0;
+            this.disableDownPanning = topLeftSide.Y >= 0;
+            this.disableUpPanning = bottomRightSide.Y >= 0;
+            
+            //Image is overflowing at edges     
+            double hTranslation = origin.X - v.X;
+            double vTranslation = origin.Y - v.Y;
+            
+            //If the translation is left to right...
+            if (v.X < 0)
+            {
+                if (this.disableRightPanning) return;
+                //If the translation has moved the left edge of the image past the left edge of the window...
+                if (topLeftSide.X + (hTranslation - tt.X) > 0)
+                {
+                    //Set the image at the left window edge
+                    hTranslation = tt.X - topLeftSide.X;
+                    this.disableRightPanning = true;
+                }
+            }
+
+            if (v.X > 0) //Right to left translation
+            {
+                if (this.disableLeftPanning) return;
+                //If the translation has moved the right edge of the image past the right edge of the window...
+                if (bottomRightSide.X + (tt.X - hTranslation) > 0)
+                {
+                    //Set the image at the right window edge
+                    hTranslation = tt.X + bottomRightSide.X;
+                    this.disableLeftPanning = true;
+                }
+            }
+
+            //Downwards translation
+            if (v.Y < 0)
+            {
+                if (this.disableDownPanning) return;
+                //If the translation has moved the top edge of the image past the top edge of the window...
+                if (topLeftSide.Y + (tt.Y - vTranslation) > 0)
+                {
+                    //Set the image at the top window edge
+                    vTranslation = tt.Y - topLeftSide.Y;
+                    this.disableDownPanning = true;
+                }
+            }
+
+            if (v.Y > 0) //Upwards translation
+            {
+                if (this.disableUpPanning) return;
+                //If the translation has moved the bottom edge of the image past the bottom edge of the window...
+                if (bottomRightSide.Y + (vTranslation - tt.Y) > 0)
+                {
+                    //Set the image at the right window edge
+                    vTranslation = tt.Y + bottomRightSide.Y;
+                    this.disableUpPanning = true;
+                }
+            }
+
+            tt.X = ignoreHTrans ? tt.X : hTranslation;
+            tt.Y = ignoreVTrans ? tt.Y : vTranslation;
         }
 
         private void image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -103,13 +193,14 @@ namespace WpfOcrInvoiceExtractor
             ScaleTransform scaleTransform = (ScaleTransform)transformGroup.Children[0];
             var tt = (TranslateTransform)transformGroup.Children[1];
             start = e.GetPosition(imageCanvas);
-            origin = new System.Windows.Point(tt.X, tt.Y);
-            var ta = invoice.TransformToAncestor(imageCanvas);
-            System.Windows.Point areaPosition = ta.Transform(new System.Windows.Point(0, 0));
-            Debug.WriteLine($"{areaPosition.X +
-                scaleTransform.ScaleX * invoice.RenderSize.Width} -- {this.Width}");
+            origin = new Point(tt.X, tt.Y);
+            var ta = invoice.TransformToAncestor(this);
+            Point areaPosition = ta.Transform(new Point(0, 0));
+            Vector topLeftSide = areaPosition - new Point(0, 0);
+            Debug.WriteLine($"{topLeftSide} -- {imageCanvas.RenderSize}");
         }
 
+        //If zooming out, must apply proper translations to re-center image
         private void image_MouseWheel(object sender, MouseWheelEventArgs e)
         {
             TransformGroup transformGroup = (TransformGroup)invoice.RenderTransform;
